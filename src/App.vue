baae95fdb6c3c8f21163d3388aec11a1ac5d0fc2
Thiago Vieira <thiiagovieir@gmail.com>
2021-02-12 23:58:43 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <BbbSeguidores msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import BbbSeguidores from './components/BbbSeguidores.vue'
+require('../node_modules/bootstrap/dist/js/bootstrap')
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
+
+export default {
+  name: 'App',
+  components: {
+    BbbSeguidores
+  }
+}
+</script>
---
+++ src/components/BbbSeguidores.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div v-for="brother in brothers" v-bind:key="brother[0].username" class="col-lg-3">
+                <div class="card mt-5">
+                    <img class="card-img-top" v-bind:src="'data:image/jpeg;base64,'+brother[0].profile_url" alt="Card image cap">
+                    <div class="card-body">
+                        <h6 class="badge bg-info">@{{ brother[0].username }}</h6>
+                        <p class="card-text">{{ brother[0].followed }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    const axios = require("axios");
+
+    export default {
+        name: 'BbbSeguidores',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                brothers: null
+            };
+        },
+        methods: {
+            getBrothers: function(){
+                axios.get('http://192.168.1.10:8000/api').then(response => (this.brothers = response.data))
+            }
+        },
+        beforeMount(){
+            console.log('https://br.vuejs.org/v2/guide/instance.html');
+        },
+        mounted(){
+            this.getBrothers();
+        }
+    }
+</script>
